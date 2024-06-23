6dbfee277801225e2eb0c5ce5508871b4068ff73
Nabeelbot1 <dknabeel413@gmail.com>
2024-06-23 14:04:38 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,9 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>◇Created By @RoronoaZo0ro
-◇Our Channel @AAnimeOngoing
-◇Thank You :</b>",
+            text = f"<b>◇Created By @RoronoaZo0ro\n◇Our Channel @AAnimeOngoing\n◇Thank You :</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
